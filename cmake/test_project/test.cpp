143e33dfb479c3dc95ef9d1d304befecac13bb73
Darrell Wright <beached@users.noreply.github.com>
2021-10-30 14:49:53 -0400

    Basic project skeleton

---
+++ cmake/test_project/test.cpp
@@ -0,0 +1,16 @@
+
+#include <daw/gumbo/gumbo_node_iterator.h>
+
+#include <cassert>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main( ) {
+	std::string_view data = R"(<html></html>)";
+	(void)data;
+	puts( "All good" );
+}
+
